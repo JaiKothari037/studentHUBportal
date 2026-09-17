d0630005c61dfdee757cd228d6c1726870572e41
Jai Kothari <25cs037@charusat.edu.in>
2026-09-17 19:58:37 +0530

    completed till practical 4 except hamburger menu

---
+++ js/scipt.js
@@ -0,0 +1,66 @@
+const togglebtn = document.getElementById('toggletheme')
+const body = document.body
+
+const currentTheme = localStorage.getItem('dark-mode')
+
+if (currentTheme == 'enabled')
+{
+    body.classList.add('dark-mode')
+}
+
+togglebtn.addEventListener('click', function() {
+    body.classList.toggle('dark-mode')
+
+    if (body.classList.contains('dark-mode'))
+    {
+        localStorage.setItem('dark-mode', 'enabled')
+    }
+    else
+    {
+        localStorage.setItem('dark-mode', 'disabled')
+    }
+})
+
+/* -------- */
+const faqquestions = document.querySelectorAll('.faqq')
+
+faqquestions.forEach(function(question) {
+    question.addEventListener('click', function() {
+        const answer = question.nextElementSibling
+        if (answer)
+        {
+            answer.classList.toggle('show')
+        }
+    })
+})
+
+/* ------- */
+
+const openModalBtn = document.getElementById('open-modal-btn')
+const closeModalBtn = document.getElementById('close-modal-btn')
+const modalOverlay = document.getElementById('timetable-modal')
+
+
+if (openModalBtn && closeModalBtn && modalOverlay)
+{
+    openModalBtn.addEventListener('click', function() {
+        modalOverlay.classList.add('show')
+    })
+
+    closeModalBtn.addEventListener('click', function() {
+        modalOverlay.classList.remove('show');
+});
+
+}
+
+/* ------ */
+const banner = document.getElementById('notificationbanner')
+const closebtn = document.getElementById('close-notif-banner')
+
+if (banner && closebtn)
+{
+    closebtn.addEventListener('click', function() {
+        banner.style.display = 'none';
+        console.log('banner close clicked')
+    });
+}
---
+++ process.php
@@ -0,0 +1,7 @@
+$name = trim($_POST["name"] ?? "");
+$email = trim($_POST["email"] ?? "");
+$password = trim($_POST["password"] ?? "");
+$confirmpassword = trim($_POST["confirmpassword"] ?? "");
+$course = trim($_POST["course"] ?? "");
+$year = trim($_POST["year"] ?? "");
+$gender = trim($_POST["gender"] ?? "");
